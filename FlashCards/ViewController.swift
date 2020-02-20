1d31191cf991db1bae357d2815c83219deaddeeb
Yasmin <yasminxhernandez@yahoo.com>
2020-02-20 14:33:44 -0800

    first commit

--- FlashCards/ViewController.swift
+++ FlashCards/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AnswerBackLabel: UILabel!
+    @IBOutlet weak var QuestionFrontLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func TappedonFlashcard(_ sender: Any) {
+        QuestionFrontLabel.isHidden = true
+        
+    }
+    
 }
 

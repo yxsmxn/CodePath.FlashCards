7337a4428337865aad371372abb5183322d3802f
Yasmin <yasminxhernandez@yahoo.com>
2020-02-20 14:19:50 -0800

    Initial Commit

---
+++ FlashCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCards
+//
+//  Created by Yasmin Hernandez on 2/20/20.
+//  Copyright © 2020 Yasmin Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

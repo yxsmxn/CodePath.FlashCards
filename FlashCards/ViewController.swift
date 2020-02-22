44b1ddaec3946ded2b66598569c8d6ba9d0b8789
Yasmin <yasminxhernandez@yahoo.com>
2020-02-21 20:10:09 -0800

    added readme

--- FlashCards/ViewController.swift
+++ FlashCards/ViewController.swift
@@ -12,15 +12,48 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var AnswerBackLabel: UILabel!
     @IBOutlet weak var QuestionFrontLabel: UILabel!
+    
+    @IBOutlet weak var card: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        AnswerBackLabel.layer.cornerRadius = 20.0
+        QuestionFrontLabel.layer.cornerRadius = 20.0
+        
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        AnswerBackLabel.layer.shadowRadius = 15.0
+        AnswerBackLabel.layer.shadowOpacity = 0.2
+        
+        QuestionFrontLabel.layer.shadowRadius = 15.0
+        QuestionFrontLabel.layer.shadowOpacity = 0.2
+        
+        AnswerBackLabel.clipsToBounds = true
+        QuestionFrontLabel.clipsToBounds = true
         // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func TappedonFlashcard(_ sender: Any) {
-        QuestionFrontLabel.isHidden = true
+        if(AnswerBackLabel.isHidden == false){
+            AnswerBackLabel.isHidden = true
+            QuestionFrontLabel.isHidden = false;
+        }
         
+        else if(QuestionFrontLabel.isHidden == true){
+            QuestionFrontLabel.isHidden = false
+            AnswerBackLabel.isHidden = true;
+        }
+        else if(QuestionFrontLabel.isHidden == false){
+            QuestionFrontLabel.isHidden = true
+            AnswerBackLabel.isHidden = false;
+        }
+        else if(AnswerBackLabel.isHidden == true){
+            AnswerBackLabel.isHidden = false
+            QuestionFrontLabel.isHidden = true;
+        }
     }
     
 }
-
--- readme.txt
+++ readme.txt
@@ -8,9 +8,9 @@
 `TODO://` Create a functional app that toggles through a question and answer.
 
 ### App Walk-though
-`TODO://` Add the URL to your animated app walk-though `gif` in the image tag below. Make sure the gif actually renders and animates when viewing this README. (☝️ Remove this paragraph after after adding gif)
+`TODO://` 
 
-<img src="YOUR_GIF_URL_HERE" width=200><br>
+<img src="FlashCards.gif" width=200><br>
 
 `TODO://` 
 
@@ -21,6 +21,6 @@
 - [X] Build in logic to show the answer side when the card is tapped
 - [X] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
+- [X] Toggle the flashcard between the question side and the answer side
+- [X] Style the question and answer side of the card to better distinguish between the two sides
 - [ ] Add selectable multiple choice answers beneath the card
